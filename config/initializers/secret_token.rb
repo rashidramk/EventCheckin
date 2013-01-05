9e78702907fc6d7e275a7b25c3154578bc3d3a19
m rashid <rashid.ciit@yahoo.com>
2013-01-05 10:10:07 +0500

    add app folder

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EventCheckin::Application.config.secret_token = '63d1149425cab88620c93092a8e9bac8043f46b6968f27fabd4404189179d6fe78c5465669c461e8e4217f357c06a78ed1364fbb756cb6a79111f30022a8c54e'
