da47c90d534d95759c654d74a72775f136859c9a
m rashid <rashid.ciit@yahoo.com>
2013-01-19 09:32:27 +0500

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -59,6 +59,7 @@ EventCheckin::Application.routes.draw do
   # just remember to delete public/index.html.
   root :to => 'home#index'
   resources :home
+  resources :home1
 
   # See how all your routes lay out with "rake routes"
 
